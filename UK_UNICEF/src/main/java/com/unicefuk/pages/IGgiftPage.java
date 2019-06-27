e51eac7d76e159ff6510a5834e45188451dc38ca
GigiRaj <gigi.raj@spericorn.com>
2019-06-27 18:56:01 +0530

    updated codes

--- UK_UNICEF/src/main/java/com/unicefuk/pages/IGgiftPage.java
+++ UK_UNICEF/src/main/java/com/unicefuk/pages/IGgiftPage.java
@@ -3,6 +3,7 @@ package com.unicefuk.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import com.unicefuk.base.BasePageObject;
 
@@ -14,6 +15,7 @@ public class IGgiftPage extends BasePageObject {
 	}
 	
 	
+	
 	/* to open IG gifts according to category from header menu */
 	public void IGgiftCategory(Integer categoryNo)throws Exception{
 		
@@ -22,5 +24,44 @@ public class IGgiftPage extends BasePageObject {
 		HoverAndClick(driver,hoverIGHeaderMenu,clickIGCateogry);
 	}
 	
+	/* to select different categories from drop down */
+	public void chooseCategory(Integer value) throws Exception{
+		
+		By categoryitem= By.id("ig-sort-category");
+		select(categoryitem, value);
+	}
+	
+	/* to select different prize range from drop down */
+	public void choosePrize(Integer value) throws Exception{
+		
+		By prizeItem= By.id("ig-sort-price");
+		select(prizeItem, value);
+	}
+	
+	/* to choose different IG gifts */
+	
+	public void chooseIGGifts(Integer randomeProductPlateNumber) throws Exception{
+		
+		 Thread.sleep(3000);
+		 By currentproductPlateField = By.xpath("//*[@class='IG-allGifts-list']/div/ul/li[" + randomeProductPlateNumber + "]/div/a");
+		 click(currentproductPlateField);
+	}
+	
+	/* to select quantity of IG gifts */
+	
+	public void selectQty(Integer value)throws Exception{
+		
+		By qty= By.id("quantityselectbox");
+		select(qty, value);
+		
+	}
+	
+	/* to click add to bag button in IG Gift detail page */
+	public void IGAddtoCart() throws Exception{
+		
+		By addtoBagButton= By.id("btn-addtocart");
+		click(addtoBagButton);
+
+	}
 
 }
